109965e12c6dcbfae10b836dce2468f8302a08e1
ChrisM-csuf <78052777+ChrisM-csuf@users.noreply.github.com>
2021-05-07 13:44:36 -0700

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,18 @@
+
+
+"""
+the program will translate a sentence that is given via user input
+we need to read the data of the .txt file
+from that data we create a laist of strings 
+use that list of strings to create a dictionary
+close the file
+
+we make a translate function that works by splitting the user input into a list of strings.
+
+once we have a list of strings, we loop through each word in the list and check it against the dictionary to see if it exists as a words that can be 
+translated
+
+replace the list items with the translated words and print the entire thing back to the user.
+"""
+
+

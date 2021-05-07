1cc47a13286be45be4ad6efad565060af5d21644
ChrisM-csuf <78052777+ChrisM-csuf@users.noreply.github.com>
2021-05-07 13:47:00 -0700

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -31,3 +31,20 @@ create_dictionary():
     return dict
 """
 
+def main():
+    sentence = input("enter a sentence: ")
+    dictionary = create_dictionary('textese.txt')
+    translate(sentence, dictionary)
+
+def create_dictionary(txt_file):
+    infile = open(txt_file, 'r')
+    words = [word.rstrip() for word in infile]
+    infile.close()
+    return dict([word.split(',') for word in words])
+
+def translate(sentence, dictionary):
+    words = sentence.split()
+    for word in words:
+        print(dictionary.get(word, word), " ", end="")
+
+main()
